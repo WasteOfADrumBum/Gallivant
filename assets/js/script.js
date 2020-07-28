c72b70e760ba0af6434e42324b0df555089cd387
WasteOfADrumBum <jmsmall89@gmail.com>
2020-07-28 16:57:00 -0400

    JS: Logo- -to-> Icon
    Make JS code for changing img from a logo to an icon on window resize for better mobile display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,26 @@
 // Change #gallivant-logo-contact from Logo -to-> Icon on window < 640px
-$(document).ready(function () {
-	if ($(window).width() > 640) {
-		$("#gallivant-logo-contact").attr(
-			"src",
-			"assets/images/gallivant-logo.png",
-		);
-	} else {
+function checkResolution() {
+	if ($(window).innerWidth() < 640) {
+		// window < 640px = Icon
 		$("#gallivant-logo-contact").attr(
 			"src",
 			"assets/images/gallivant-icon.png",
 		);
+	} else {
+		// window > 640px = Logo
+		$("#gallivant-logo-contact").attr(
+			"src",
+			"assets/images/gallivant-logo.png",
+		);
 	}
+}
+
+// Check img on window resize
+$(window).resize(function () {
+	checkResolution();
+});
+
+// Check img on window load
+$(document).ready(function () {
+	checkResolution();
 });
