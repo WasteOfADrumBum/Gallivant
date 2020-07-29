03c458d511032cf10e084a84154deaccee1233a7
WasteOfADrumBum <jmsmall89@gmail.com>
2020-07-29 18:19:23 -0400

    formData Array
    Captured index.html form into an array (formData) to be used on results.html page to use for API code

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,32 @@ $(window).resize(function () {
 $(document).ready(function () {
 	checkResolution();
 });
+
+/* Captures index.html formData to be used on results.html */
+$(document).ready(function () {
+	// Instead of listening to button click, always listen to form submit event
+	$("#loc-date-form").submit(function () {
+		// Create an array of objects called `formData`
+		var formData = $(this)
+			.find(":input")
+			.map(function () {
+				return {
+					name: $(this).attr("name"),
+					value: $(this).val(),
+				};
+			})
+			.get();
+
+		// Store it in localStorage
+		localStorage.setItem("formData", JSON.stringify(formData));
+	});
+
+	// Read and parse from localStorage
+	var formData = JSON.parse(localStorage.getItem("formData"));
+
+	// Iterate through array, and set HTML of matching <span> element
+	$.each(formData, function (i, datum) {
+		$("#display_" + datum.name).html(datum.value);
+	});
+	console.log("formData Array:", formData);
+});
